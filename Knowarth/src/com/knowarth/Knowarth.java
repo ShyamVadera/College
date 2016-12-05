a86545065112fda562a0c03303fc3c9bf8668006
ShyamVadera <vaderashyam93@gmail.com>
2016-12-05 18:12:40 +0530

    Knowarth  6 commit

--- Knowarth/src/com/knowarth/Knowarth.java
+++ Knowarth/src/com/knowarth/Knowarth.java
@@ -4,7 +4,7 @@ public class Knowarth {
 
 	public static void main(String[] args) {
 	
-		System.out.println("hello knowarth");
+		System.out.println("hello");
 		System.out.println("hello world");
 		
 	}
