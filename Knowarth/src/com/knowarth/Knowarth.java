9ae1909c71e101a20a006e10627d02c2b9c015af
shyam.vadera <shyam.vaderaknowarth.com>
2016-12-05 18:34:14 +0530

    knowarth 9 commit

--- Knowarth/src/com/knowarth/Knowarth.java
+++ Knowarth/src/com/knowarth/Knowarth.java
@@ -4,9 +4,9 @@ public class Knowarth {
 
 	public static void main(String[] args) {
 		int y=10;
-		y=10+5;
-		System.out.println(y);
-		System.out.println("hello"+y);
+		
+		System.out.println("Hello how r u"+y);
+		System.out.println("hello bye"+y);
 	}
 
 }
